4cc359cccadaffab0fc1712557243ff6f01a2f32
Milan Mishra <milanmishr20@gmail.com>
2023-09-08 21:39:04 -0500

    Dataset update

--- HAB_Detect.py
+++ HAB_Detect.py
@@ -13,11 +13,11 @@ for image_class in os.listdir(data_dir):
         img = cv2.imread(image_path)
         resize = tf.image.resize(img, (256,256))
         predictVal = HAB_Detect.predict(np.expand_dims(resize/255, 0))
-        if predictVal > 0.45: 
-            print('Predicted class likely has an algae bloom {}'.format(image_path))
+        if predictVal > 0.4: 
+            print('Predicted class likely has an algae bloom {}'.format(image_path) + predictVal)
             with open('pab.txt', 'a') as f:
                 f.write(image_path + '\n')
         else:
-            print('Predicted class likely does not have an algae bloom {}'.format(image_path))
+            print('Predicted class likely does not have an algae bloom {}'.format(image_path) + predictVal)
             with open('npab.txt', 'a') as f:
                 f.write(image_path + '\n')
